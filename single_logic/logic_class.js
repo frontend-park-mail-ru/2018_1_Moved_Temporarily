b7d704c85bb2d676d89d590c585faae5c832e8b5
Unknown <astahov.dl@mail.ru>
2018-02-18 05:46:06 +0300

    opponent map fully generating

--- single_logic/logic_class.js
+++ single_logic/logic_class.js
@@ -40,16 +40,89 @@ class SingleLogic {
             }
         map.push(row);
         };
-        /*let randomizate = function() { return Math.floor(Math.random() * 10)};
-        for (let i = 0; i < 3; ++i){
-            map[randomizate()][randomizate()] = 1;
-        }*/
 
+        let randomizate = function() { return Math.floor(Math.random() * 10)};
+
+
+        for(let decker = 1; decker <= 4; decker++) {
+            let counter = 0;
+            while (counter < 4 - decker + 1) {
+                let xpoint = randomizate();
+                let ypoint = randomizate();
+
+                let direction = this._isAbleSeveralDeckerPoint(map, xpoint, ypoint, decker);
+                switch (direction) {
+                    case "left" :
+                        for (let i = xpoint; i > xpoint - decker; --i) {
+                            map[i][ypoint] = decker;
+                        }
+                        counter++;
+                        break;
+                    case "right" :
+                        for (let i = xpoint; i < xpoint + decker; ++i) {
+                            map[i][ypoint] = decker;
+                        }
+                        counter++;
+                        break;
+                    case "top" :
+                        for (let j = ypoint; j < ypoint + decker; ++j) {
+                            map[xpoint][j] = decker;
+                        }
+                        counter++;
+                        break;
+                    case "down" :
+                        for (let j = ypoint; j > ypoint - decker; --j) {
+                            map[xpoint][j] = decker;
+                        }
+                        counter++;
+                        break;
+                }
+            }
+        }
         return map;
     };
 
-}
+    _isAbleSeveralDeckerPoint(map, xpoint, ypoint, deckerSize){
+        /* left, right, down, top borders */
+        let leftGroup = [((xpoint - deckerSize) === -1) ? 0 : (xpoint - deckerSize),
+            ((xpoint + 1) === 10) ? 9 : (xpoint + 1),
+            ((ypoint - 1) === -1) ? 0 : (ypoint - 1),
+            ((ypoint + 1) === 10) ? 9 : (ypoint + 1)];
+        let rightGroup = [((xpoint - 1) === -1) ? 0 : (xpoint - 1),
+            ((xpoint + deckerSize) === 10) ? 9 : (xpoint + deckerSize),
+            ((ypoint - 1) === -1) ? 0 : (ypoint - 1),
+            ((ypoint + 1) === 10) ? 9 : (ypoint + 1)];
+        let downGroup = [((xpoint - 1) === -1) ? 0 : (xpoint - 1),
+            ((xpoint + 1) === 10) ? 9 : (xpoint + 1),
+            ((ypoint - deckerSize) === -1) ? 0 : (ypoint - deckerSize),
+            ((ypoint + 1) === 10) ? 9 : (ypoint + 1)];
+        let upGroup = [((xpoint - 1) === -1) ? 0 : (xpoint - 1),
+            ((xpoint + 1) === 10) ? 9 : (xpoint + 1),
+            ((ypoint - 1) === -1) ? 0 : (ypoint - 1),
+            ((ypoint + deckerSize) === 10) ? 9 : (ypoint + deckerSize)];
 
+        let groups = {"left" : leftGroup, "right" : rightGroup, "down" : downGroup, "up" : upGroup};
+        //groups.sort(function(){return Math.floor(Math.random())});
+        let trueGroups = [];
+        for (let group in groups){
+            if (this._isAbleConcreteDecker(map, groups[group])){
+                trueGroups.push(group);
+            }
+        }
+        return trueGroups.length > 0 ? trueGroups[Math.floor(Math.random() * 10) % trueGroups.length] : 'none';
+    }
+
+    _isAbleConcreteDecker(map, group){
+        for (let i = group[0]; i <= group[1]; ++i) {
+            for (let j = group[2]; j <= group[3]; ++j) {
+                if (i < 0 || i > 9 || j < 0 || j > 9 || map[i][j] !== 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
 
 const single_logic = new SingleLogic();
 single_logic.getMap();
