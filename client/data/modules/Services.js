b35c1d8d7efcf2a401e97aed8af6d50624c4c481
Unknown <astahov.dl@mail.ru>
2018-03-23 02:15:50 +0300

    starting to change to mvc model

--- client/data/modules/Services.js
+++ client/data/modules/Services.js
@@ -4,69 +4,53 @@
  * @module Services
  */
 
-(function()
-{
-    const Http = document.Http;
+import http from "./http/http.js";
 
-    class Services
-    {
-        static placeItem(element)
-        {
-            element.style.left = (Math.random() * 100) + "%";
-            element.style.top = (Math.random() * 100) + "%";
-            element.style.position = "absolute";
-        }
+class Services {
+    static placeItem(element) {
+        element.style.left = (Math.random() * 100) + "%";
+        element.style.top = (Math.random() * 100) + "%";
+        element.style.position = "absolute";
+    }
 
-        static changeMenu(newMenu)
-        {
-            document.currentMenu.hide();
-            document.currentMenu = newMenu;
-            newMenu.show();
+    static changeMenu(newMenu) {
+        document.currentMenu.hide();
+        document.currentMenu = newMenu;
+        newMenu.show();
 
-        }
+    }
 
-        static logout()
-        {
-            document.currentUser = null;
-            http.fetchGet("/api/user/logout").
-                then((response) => {});
-        }
+    static logout() {
+        document.currentUser = null;
+        http.fetchGet("/api/user/logout").then((response) => {
+        });
+    }
 
-        static serverCheck(body)
-        {
-            return http.fetchPost("/api/user/login", body);
-        }
+    static serverCheck(body) {
+        return http.fetchPost("/api/user/login", body);
+    }
 
-        static changeUser(user)
-        {
-            http.fetchGet("/api/user/info")
-                .then((response) =>
-            {
+    static changeUser(user) {
+        http.fetchGet("/api/user/info")
+            .then((response) => {
                 document.currentUser = response;
                 Services.changeMenu(document.mainMenu);
                 document.profileBlock.changeUser(response);
             })
-                .catch((response) =>
-            {
+            .catch((response) => {
                 document.currentUser = null;
             });
-        }
-
-        static checkRegister(body)
-        {
-            return http.fetchPost("/api/user/signup", body);
-        }
-
-        static getLeaders()
-        {
-            return http.fetchGet("/api/user/score");
-        }
-
-        static getAboutText()
-        {
-            return http.fetchGet("/api/user/about");
-        }
     }
 
-    document.Services = Services;
-})();
+    static checkRegister(body) {
+        return http.fetchPost("/api/user/signup", body);
+    }
+
+    static getLeaders() {
+        return http.fetchGet("/api/user/score");
+    }
+
+    static getAboutText() {
+        return http.fetchGet("/api/user/about");
+    }
+}
