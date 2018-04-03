00044d05277f2823e9dba0bf745c9658f05cbf76
Unknown <astahov.dl@mail.ru>
2018-04-03 03:56:03 +0300

    remove old schemes

--- client/data/modules/views/LeaderboardView/LeaderboardView.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-import BaseView from "../BaseView/BaseView.js";
-
-const leaderboard = new BaseView(document.body, generateLeaderboardView, {title: "Leaderboard", players: []});
-
-export default leaderboard;
