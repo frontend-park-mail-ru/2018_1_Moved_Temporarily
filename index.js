e101922757ae22019696bc5d71753877e5c05858
Unknown <astahov.dl@mail.ru>
2018-04-12 00:37:20 +0300

    repair cors

--- index.js
+++ index.js
@@ -46,6 +46,61 @@ const users = {
 
 const aboutText = {text: ["Counter strike golang", "Moved Temporarily"]};
 
+const allowedOrigins = [
+    'localhost',
+    'https://moved-temporarily-back.herokuapp.com',
+    'https://moved-temporarily-front.herokuapp.com',
+    '//moved-temporarily-front.herokuapp.com',
+];
+
+const CORS_HEADERS = {
+    requestedHeaders: 'Access-Control-Request-Headers'.toLowerCase(),
+    requestedMethod: 'Access-Control-Request-Method'.toLowerCase(),
+    allowOrigin: 'Access-Control-Allow-Origin'.toLowerCase(),
+    allowMethods: 'Access-Control-Allow-Methods'.toLowerCase(),
+    allowHeaders: 'Access-Control-Allow-Headers'.toLowerCase(),
+    allowCredentials: 'Access-Control-Allow-Credentials'.toLowerCase(),
+};
+
+app.use(function (req, res, next) {
+    const requestOrigin = req.headers['origin'];
+    //console.log(req.headers);
+    console.log(requestOrigin);
+
+    if (typeof requestOrigin !== 'undefined') {
+        const requestOriginHostname = url.parse(requestOrigin).hostname;
+
+
+        const requestedHeaders = req.headers[CORS_HEADERS.requestedHeaders];
+        const requestedMethod = req.headers[CORS_HEADERS.requestedMethod];
+        logger(`Requested ${req.method} ${req.path} with origin ${requestOrigin} (${requestOriginHostname})`, {
+            requestedHeaders,
+            requestedMethod,
+        });
+
+        const headers = [];
+        if (requestedHeaders) {
+            headers.push([CORS_HEADERS.allowHeaders, requestedHeaders]);
+        }
+        if (requestedMethod) {
+            headers.push([CORS_HEADERS.allowMethods, 'GET, POST, OPTIONS']);
+        }
+
+        // res.setHeader(CORS_HEADERS.allowOrigin, '*');
+
+        if (allowedOrigins.includes(requestOriginHostname)) {
+            headers.push([CORS_HEADERS.allowOrigin, requestOrigin]);
+            headers.push([CORS_HEADERS.allowCredentials, 'true']);
+        }
+
+        const result = headers.map(pair => '\t' + pair.join(': ')).join('\n');
+        logger(`Response with headers:\n` + result);
+
+        headers.forEach(([name, value]) => res.setHeader(name, value));
+    }
+    next();
+});
+
 app.post("/api/user/login", function(req, res)
 {
     let password = req.body.password;
